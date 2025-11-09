bd9e29bbdddd3622fcb7b3abba52a6d7240c2d02
Nikita <zhnikita3@gmail.com>
2025-11-10 00:20:23 +0300

    add: Добавлена первоначальная реализация пула потоков

---
+++ inc/threadpool.hpp
@@ -0,0 +1,115 @@
+#ifndef THREADPOOL_HPP__
+#define THREADPOOL_HPP__
+
+#include <thread>
+#include <vector>
+#include <queue>
+#include <condition_variable>
+#include <semaphore>
+#include <mutex>
+#include <future>
+#include <functional>
+
+class ThreadPool
+{
+public:
+    explicit ThreadPool(size_t nWorkers)
+    {
+        if (nWorkers == 0)
+            throw std::runtime_error("nWorkers should be > 0!");
+
+        workers_.reserve(nWorkers);
+
+        for (size_t i = 0; i < nWorkers; ++i)
+            workers_.emplace_back([this] { Worker(); });
+    }
+
+    ThreadPool()
+        : ThreadPool(std::thread::hardware_concurrency()) {}
+
+    ~ThreadPool() noexcept
+    {
+        Stop();
+
+        for (auto &w : workers_)
+            if (w.joinable())
+                w.join();
+    }
+
+    template <typename Func, typename ...Args>    
+    auto AddTask(Func &&func, Args &&... args)
+        -> std::future<std::invoke_result_t<Func, Args...>>
+    {
+        using Ret = std::invoke_result_t<Func, Args...>;
+
+        std::packaged_task<Ret()> task(
+            [func = std::forward<Func>(func), args_tuple = std::make_tuple(std::forward<Args>(args)...)]
+            {
+                return std::apply(
+                    [&](auto &&...captured_args) -> Ret {
+                        return std::invoke(func, std::forward<decltype(captured_args)>(captured_args)...);
+                    },
+                    std::move(args_tuple)
+                );
+            }
+        );
+        std::future<Ret> fut = task.get_future();
+
+        {
+            std::lock_guard l(queueMutex_);
+            if (stopRequired_)
+                throw std::runtime_error("ThreadPool is stopping or stopped");
+
+            taskQueue_.emplace([task = std::move(task)] mutable { task(); });
+        }
+        
+        poolNotifier_.notify_one();
+
+        return fut;
+    }
+
+    void Stop()
+    {
+        {
+            std::lock_guard l(queueMutex_);
+            stopRequired_ = true;
+        }
+
+        poolNotifier_.notify_all();
+    }
+
+private:
+    void Worker()
+    {
+        while (true)
+        {
+            std::packaged_task<void()> task;
+
+            {
+                std::unique_lock l(queueMutex_);
+                poolNotifier_.wait(l, 
+                    [this] -> bool { return !taskQueue_.empty() || stopRequired_; }   
+                );
+
+                if (stopRequired_)
+                    return;
+
+                task = std::move(taskQueue_.front());
+                taskQueue_.pop();
+            }
+
+            task();
+        }
+    }
+
+    std::condition_variable poolNotifier_;
+
+    mutable std::mutex queueMutex_;
+    std::queue<std::packaged_task<void()>> taskQueue_;
+
+    std::vector<std::thread> workers_;
+
+    bool stopRequired_{false};
+};
+
+#endif // THREADPOOL_HPP__
---
+++ src/main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <chrono>
+#include "threadpool.hpp"
+
+int main()
+{
+    using namespace std::chrono_literals;
+    ThreadPool pool(5);
+    std::vector<std::future<std::thread::id>> v;
+
+    std::mutex mut;
+
+    auto startTime = std::chrono::high_resolution_clock::now();
+
+    for (int i = 0; i < 10; ++i)
+        v.push_back(pool.AddTask(
+            [&mut]
+            {
+                {
+                    std::lock_guard l(mut);
+                    std::cout << "hello from: " << std::this_thread::get_id() << '\n' << std::flush;
+                }
+                std::this_thread::sleep_for(5s);
+                return std::this_thread::get_id();
+            }
+        ));
+
+    for (auto &f : v)
+        f.wait();
+
+    auto endTime = std::chrono::high_resolution_clock::now();
+
+    for (auto &f : v)
+        std::cout << "result: " << f.get() << '\n';
+
+    float timeElapsed = std::chrono::duration_cast<std::chrono::milliseconds>(endTime - startTime).count();
+    std::cout << "Time elapsed: " << timeElapsed / 1000.0 << " s.\n";
+
+    return 0;
+}
